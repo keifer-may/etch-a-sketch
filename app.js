044214ae190cef30b586d258c9f37a9fd5b718eb
Keifer May <dkmay3@gmail.com>
2022-10-27 17:29:38 -0400

    Add buttons and new functions for user control

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 const content = document.querySelector(".content");
 const canvas = document.querySelector(".container");
-const color = "black";
 const instructions = document.querySelector(".instructions");
-// const gridInputAttributes = {
-//   type: "range",
-//   name: "grid",
-//   min: "16",
-//   max: "100",
-//   step: "4",
-//   defaultValue: "16",
-// }
-
-// need to determine how to use input objects to interact with the canvas and the game--
-// fix the gridInput tools so that the uuser can edit the number of pixels in the canvas
-
+const numPixelsUpdate = document.querySelector('#update-grid')
+const eraseButton = document.querySelector('#erase-grid');
 
 let drawing = false;
-let numItems = '16';
-
-// function setAttributes(element, attributes) {
-//   Object.keys(attributes).forEach(attr => {
-//     element.setAttribute(attr, attributes[attr]);
-//   });
-// }
-
-
-// let gridDiv = document.createElement('div');
-// let gridInput = document.createElement('input');
-// gridDiv.appendChild(content);
-// gridInput.appendChild(gridDiv);
-
-// setAttributes(gridInput, gridInputAttributes);
-
+let numPixels = '16';
+const color = "black";
 
 function createGrid(width) {
   for (let i = 0; i < width * width; i++) {
@@ -42,32 +17,68 @@ function createGrid(width) {
     "style",
     `grid-template-columns: repeat(${width}, auto); 
   grid-template-rows: repeat(${width}, auto);`
-  );
-  const pixels = canvas.children;
+);}
+
+function createPixels() {
+  let pixels = canvas.children;
   for (let i = 0; i < pixels.length; i++) {
     pixels[i].setAttribute("class", "pixel");
   }
 }
 
-createGrid(parseInt(numItems));
+function clearGrid() {
+  for (let i = 0; i < numPixels * numPixels; i++) {
+    let pixel = document.querySelector('.pixel');
+    pixel.remove();
+  } 
+}
 
-const pixels = canvas.childNodes;
+function eraseCanvas() {
+  clearGrid();
+  createGrid(parseInt(numPixels));
+  createPixels();
+}
+
+createGrid(parseInt(numPixels));
+createPixels();
+
+numPixelsUpdate.addEventListener('click', (e) => {
+  let userPixel = prompt("Please provide a number of pixels that we can use as the width for the canvas. Use only numerals and please choose a number between 16 and 100:");
+  if (parseInt(userPixel) < 16) {
+    userPixel = 16;
+  }
+  else if (parseInt(userPixel) > 100) {
+    userPixel = 100;
+  }
+  console.log(userPixel);
+  clearGrid();
+  createGrid(parseInt(userPixel));
+  createPixels();
+  numPixels = userPixel;
+})
+
+eraseButton.addEventListener('click', (e) => {
+  eraseCanvas();
+})
+
 
 function draw() {
-  pixels.forEach((pixel) => {
+  const pixels = canvas.children;
+  [].forEach.call(pixels, (pixel) => {
     pixel.addEventListener("mouseover", (v) => {
-      if (drawing === true) {
+      if (drawing == true) {
+        console.log('foreach');
         pixel.setAttribute("style", `background-color: ${color};`);
-      }
-    });
-  });
-}
+      }});
+    });  
+};
 
 canvas.addEventListener("mousedown", (e) => {
   instructions.style.visibility = 'hidden';
-  
   drawing = true;
+  console.log(drawing);
   draw();
+  console.log('called function');
 });
 
 canvas.addEventListener("mouseup", (e) => {
